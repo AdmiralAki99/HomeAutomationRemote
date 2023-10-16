e8bfc0bcb9b061f557136b03176d2fe5d88880c9
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-10-17 00:19:30 +0530

    Added Material UI Buttons
    Added material ui buttons to check responsiveness

--- smart-remote/src/App.tsx
+++ smart-remote/src/App.tsx
@@ -1,33 +1,63 @@
 import React ,{useState, useEffect} from 'react';
-import logo from './logo.svg';
-import { Button } from "./@/components/ui/button";
-import { ChevronRightIcon } from "@radix-ui/react-icons"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,} from './@/components/ui/card'
+import { useTheme } from '@mui/material/styles';
+import { Button,IconButton,Box,Card,CardContent,Grid,CardMedia,Paper,Slide,MobileStepper,Typography} from '@mui/material';
+import {ChevronRight,ChevronLeft} from '@mui/icons-material';
+import Carousel from 'react-material-ui-carousel'
 
 function App() {
-  const [selected,setSelected] = React.useState(0);
+  const theme = useTheme();
+  var items = [
+    {
+        name: "Random Name #1",
+        description: "Probably the most random thing you have ever seen!"
+    },
+    {
+        name: "Random Name #2",
+        description: "Hello World!"
+    }
+  ]
   return (
-    <div className="pl-4 pr-4 pt-6">
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 gap-4 ">
-          <Card className="w-[300px] h-[100px]">
-            <div className='mx-auto grid grid-cols-3 lg:col-span-2 '>
-              <div className='bg-red-500 max-w-7xl'>
-                <Button><ChevronRightIcon className="h-4 w-4" /></Button>
-              </div>
-            </div>
-          </Card>
-          <Card className="w-[300px] h-[100px]"></Card>
-          <Card className="w-[300px] h-[100px]"></Card>
-        </div>
-      </div>
-    </div>
+    <Grid container justifyContent="center">
+      <Carousel>
+        {
+           
+        }
+      </Carousel>
+      <Box sx={{ flexDirection: "column" }}>
+        <Card sx={{ display: "inline-flex", width: "auto", bgcolor: 'black' }}>
+          <Box sx={{ display: "flex", flexDirection: "column"}}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <IconButton>
+                <ChevronRight />
+              </IconButton>
+            </CardContent>
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <IconButton>
+                <ChevronRight />
+              </IconButton>
+            </CardContent>
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <IconButton>
+                <ChevronRight sx={{color: 'white'}}/>
+              </IconButton>
+            </CardContent>
+          </Box>
+        </Card>
+        <Card>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <IconButton>
+                <ChevronRight />
+              </IconButton>
+            </CardContent>
+          </Box>
+        </Card>
+      </Box>
+    </Grid>
   );
 }
 
