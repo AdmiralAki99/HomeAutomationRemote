058e8a7c24fd5f2a608e16de5e1a09378fbc1083
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-10-22 16:18:46 +0530

    Changed Light Screen UI
    Changed master light bar ui, and created smaller light bars for each light that can be individually regulated

--- smart-remote/src/App.tsx
+++ smart-remote/src/App.tsx
@@ -9,6 +9,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {View} from 'react-native';
 import RangeSlider from './components/RangeSlider';
+import LightSlider from './components/LightSlider';
 
 function HomeScreen(){
   return (
@@ -201,43 +202,79 @@ function HomeScreen(){
 function LightScreen(){
   return (
     <View style={{ alignItems: "center" }}>
-      <div className="bg-home w-full max-h-screen">
+      <div className="bg-home w-full h-screen">
         <div className="pb-16">
           <div className="flex items-center justify-center">
-            <div className="grid grid-cols-2 items-center bg-red-300 justify-center gap-6 p-8">
+            <div className="grid grid-cols-2 items-center bg-home justify-center gap-6 p-8">
               <div className="grid grid-cols-1 items-center justify-center gap-4 p-6">
                 <div className="flex items-center mb-4 gap-6">
                   <input
                     id="light-section-1"
-                    type="radio"
+                    type="checkbox"
+                    value={""}
+                    name="light-selection"
+                    className="w-4 h-4 text-white bg-noir border-white focus: ring-white"
+                  ></input>
+                  <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-800 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
+                    <div className="grid grid-rows-3 grid-cols-2 gap-4">
+                      <div className="row row-start-1 row-end-3 text-white">
+                        Lamp
+                      </div>
+                      <div className="flex row-start-2 row-end-4 items-center justify-center h-full w-full object-fill pt-6 pl-6">
+                        <LightSlider
+                          min={0}
+                          onChange={({ min }: { min: number }) =>
+                            console.log(`val = ${min}`)
+                          }
+                        ></LightSlider>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="flex items-center mb-4 gap-6">
+                  <input
+                    id="light-section-1"
+                    type="checkbox"
+                    value={""}
+                    name="light-selection"
+                    className="w-4 h-4 text-white bg-black border-white focus: ring-white"
+                  ></input>
+                  <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-800 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
+                    <div className="grid grid-rows-3 grid-cols-2 gap-4">
+                      <div className="row row-start-1 row-end-3 text-white">
+                        Lamp
+                      </div>
+                      <div className="flex row-start-2 row-end-4 items-center justify-center h-full w-full object-fill pt-6 pl-6">
+                        <LightSlider
+                          min={0}
+                          onChange={({ min }: { min: number }) =>
+                            console.log(`val = ${min}`)
+                          }
+                        ></LightSlider>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+                <div className="flex items-center mb-4 gap-6">
+                  <input
+                    id="light-section-1"
+                    type="checkbox"
                     value={""}
                     name="light-selection"
                     className="w-4 h-4 text-white bg-black border-white focus: ring-white"
                   ></input>
                   <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
                     <div className="grid grid-rows-3 grid-cols-2 gap-4">
-                      <div className="row row-start-1 row-end-3">Lamp</div>
-                      <div className="flex row-start-2 row-end-4 items-center justify-center h-full w-full object-fill pt-4 bg-white"></div>
-                    </div>
-                  </a>
-                </div>
-                <div className="flex items-center mb-4 gap-6">
-                  <input
-                    id="light-section-1"
-                    type="radio"
-                    value={""}
-                    name="light-selection"
-                    className="w-4 h-4 text-white bg-black border-white focus: ring-white"
-                  ></input>
-                  <a className="block max-w-lg w-80 h-40 p-6 bg-yellow-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
-                    <div className="grid grid-rows-3 grid-cols-2 gap-4">
-                      <div className="row row-start-1 row-end-3">01</div>
-                      <div className="row row-start-1 row-end-3 items-center bg-black mb-auto">
-                        <h5 className="text-3xl font-bold tracking-tight text-white">
-                          Card Title
-                        </h5>
-                        <p className="text-white"> Card Content</p>
-                        <p> Card Content</p>
+                      <div className="row row-start-1 row-end-3 text-white">
+                        Lamp
+                      </div>
+                      <div className="flex row-start-2 row-end-4 items-center justify-center h-full w-full object-fill pt-6 pl-6">
+                        <LightSlider
+                          min={0}
+                          onChange={({ min }: { min: number }) =>
+                            console.log(`val = ${min}`)
+                          }
+                        ></LightSlider>
                       </div>
                     </div>
                   </a>
@@ -245,41 +282,23 @@ function LightScreen(){
                 <div className="flex items-center mb-4 gap-6">
                   <input
                     id="light-section-1"
-                    type="radio"
+                    type="checkbox"
                     value={""}
                     name="light-selection"
                     className="w-4 h-4 text-white bg-black border-white focus: ring-white"
                   ></input>
-                  <a className="block max-w-lg w-80 h-40 p-6 bg-yellow-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
+                  <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
                     <div className="grid grid-rows-3 grid-cols-2 gap-4">
-                      <div className="row row-start-1 row-end-3">01</div>
-                      <div className="row row-start-1 row-end-3 items-center bg-black mb-auto">
-                        <h5 className="text-3xl font-bold tracking-tight text-white">
-                          Card Title
-                        </h5>
-                        <p className="text-white"> Card Content</p>
-                        <p> Card Content</p>
+                      <div className="row row-start-1 row-end-3 text-white">
+                        Lamp
                       </div>
-                    </div>
-                  </a>
-                </div>
-                <div className="flex items-center mb-4 gap-6">
-                  <input
-                    id="light-section-1"
-                    type="radio"
-                    value={""}
-                    name="light-selection"
-                    className="w-4 h-4 text-white bg-black border-white focus: ring-white"
-                  ></input>
-                  <a className="block max-w-lg w-80 h-40 p-6 bg-yellow-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
-                    <div className="grid grid-rows-3 grid-cols-2 gap-4">
-                      <div className="row row-start-1 row-end-3">01</div>
-                      <div className="row row-start-1 row-end-3 items-center bg-black mb-auto">
-                        <h5 className="text-3xl font-bold tracking-tight text-white">
-                          Card Title
-                        </h5>
-                        <p className="text-white"> Card Content</p>
-                        <p> Card Content</p>
+                      <div className="flex row-start-2 row-end-4 items-center justify-center h-full w-full object-fill pt-6 pl-6">
+                        <LightSlider
+                          min={0}
+                          onChange={({ min }: { min: number }) =>
+                            console.log(`val = ${min}`)
+                          }
+                        ></LightSlider>
                       </div>
                     </div>
                   </a>
