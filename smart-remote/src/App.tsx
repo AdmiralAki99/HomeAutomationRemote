091c7ee1eb38a29bd54a7bdb128e5412581541e4
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-10-23 14:27:02 +0530

    Added Light Card To Screen
    Added light card to screen in order to test it out

--- smart-remote/src/App.tsx
+++ smart-remote/src/App.tsx
@@ -11,6 +11,7 @@ import {View} from 'react-native';
 import RangeSlider from './components/RangeSlider';
 import LightSlider from './components/LightSlider';
 import Checkbox from './components/LightCheckbox';
+import LightCard from './components/LightCard';
 
 function HomeScreen(){
   return (
@@ -265,7 +266,8 @@ function LightScreen(){
                     name="light-selection"
                     className="w-4 h-4 text-white bg-black border-white focus: ring-white"
                   ></input>
-                  <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
+                  {/* Updated the Basic Template that is made into Light Card, need to delete after checking functionality*/}
+                  {/* <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
                     <div className="grid grid-rows-3 grid-cols-2 gap-4">
                       <div className="row row-start-1 row-end-3 text-white">
                         Lamp
@@ -279,7 +281,14 @@ function LightScreen(){
                         ></LightSlider>
                       </div>
                     </div>
-                  </a>
+                  </a> */}
+                  <LightCard
+                    min={0}
+                    label="Computer"
+                    onChange={({ min }: { min: number }) =>
+                      console.log(`val = ${min}`)
+                    }
+                  />
                 </div>
                 <div className="flex items-center mb-4 gap-6">
                   <input
