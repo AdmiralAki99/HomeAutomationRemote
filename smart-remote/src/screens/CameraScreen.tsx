b82576b5c6e041a8659224ce2b04008fc405c4f3
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-10-31 15:11:30 +0530

    Added Camera Screen
    Added Camera Screen template need to add the video player functionality

--- smart-remote/src/screens/CameraScreen.tsx
+++ smart-remote/src/screens/CameraScreen.tsx
@@ -1,6 +1,23 @@
+import {View} from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import { ScreenNavbar } from '../components/Navbar';
+import { ScreenParamList } from '../App';
 
-function CameraScreen(){
+type CameraProps = NativeStackScreenProps<ScreenParamList, 'Camera'>;
+
+function CameraScreen({route, navigation} : CameraProps){
+    navigation.navigate('Camera')
+
+    return(
+        <View>
+            {/* <h1>Test Screen</h1>
+            <Button onClick={() => navigation.navigate('Light')}>
+                Light Screen
+            </Button> */}
+            <ScreenNavbar navigation={navigation} destination={"Home"} />
+        </View>
+    )
 
 }
 
