4ef40d7acc73c3748b045ede74e4096a3680a766
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-11-06 13:40:18 +0530

    Added Axios To Manage PUT Request
    Added Axios to manage put request, also changed the data structure of the way the lights are stored and tested UI creation to show details of obtained lights

--- smart-remote/src/screens/TestScreen.tsx
+++ smart-remote/src/screens/TestScreen.tsx
@@ -9,49 +9,60 @@ import { ScreenParamList } from '../App';
 import { HomeScreenNavbar, LightScreenNavBar, ScreenNavbar } from '../components/Navbar';
 import LightCard from '../components/LightCard';
 import { light } from '@mui/material/styles/createPalette';
+import axios from 'axios';
+import { error } from 'console';
 
 type testProps = NativeStackScreenProps<ScreenParamList, 'Test'>;
 
 function TestScreen({route, navigation} : testProps){
     navigation.navigate('Test')
-    const [data,setData] = useState<Record<string,any>>([{}])
+    const [data,setData] = useState<Record<string,any>>([])
 
   const handleClick = async () =>{
-    await fetch("/light/1",{method:'GET'}).then(
+    await fetch("/light/all",{method:'GET'}).then(
       res => res.json()
     ).then(
-      data => {
-        setData(data)
+      val => {
+        setData(val)
       }
     )
   }
+
+  const isEmpty = (val: any) => val == null || !(Object.keys(val) || val).length;
 
   const updateLight = async () =>{
-    await fetch("/light/1",{method:'POST', body:JSON.stringify({state:'off'})}).then(
-      res => res.json()
-    ).then(
-      data => {
-        // setData(data)
-        console.log(data)
-      }
-    )
+    // await fetch("/light/3",{method:'PUT',body: JSON.stringify({state:"off"})}).then(
+    //   res => res.json()
+    // ).then(
+    //   data => {
+    //     // setData(data)
+    //     console.log(data)
+    //   }
+    // )
+    axios.put("/light/3",{state:"off"}).then(
+      response => console.log(response)
+    ).catch(error => console.log(error))
   }
 
-    return(
-        <View>
-            {/* <h1>Test Screen</h1>
+    return (
+      <View>
+        {/* <h1>Test Screen</h1>
             <Button onClick={() => navigation.navigate('Light')}>
                 Light Screen
             </Button> */}
-            <LightScreenNavBar navigation={navigation} destination={"Home"} />
-            <Button onClick={updateLight}>Press This</Button>
-            {/* {
-              data? Object.keys(data).map((key)=>(
-                <p>{key}: {data[key]}</p>
-              )) : <h1>loading</h1>
-            } */}
-        </View>
-    )
+        <LightScreenNavBar navigation={navigation} destination={"Home"} />
+        <Button onClick={handleClick}>Press This</Button>
+        {isEmpty(data) === true ? (
+          <h1>Loading....</h1>
+        ) : (
+          data.map((light: any, index: any) => (
+            <div key={index}>
+              <h1>{light["name"]}</h1>
+            </div>
+          ))
+        )}
+      </View>
+    );
 
 }
 
