1aa70e53ab36a845797fca5f388c39c665560a5d
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-11-17 21:54:32 +0530

    Fixed Calendar Component Appearance
    Added component appearance to make it look more presentable. Need to add event showings

--- smart-remote/src/components/Calendar.tsx
+++ smart-remote/src/components/Calendar.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import format from "date-fns/format"
 import addMonths from "date-fns/addMonths";
+import isSameMonth from "date-fns/isSameMonth";
 import subMonths from "date-fns/subMonths";
 import addYears from "date-fns/addYears";
 import subYears from "date-fns/subYears";
+import startOfWeek from "date-fns/startOfWeek";
+import endOfWeek from "date-fns/endOfWeek";
+import endOfMonth from "date-fns/endOfMonth";
+import addDays from "date-fns/addDays";
 
 import './Calendar.css'
+import { isSameDay, startOfMonth } from "date-fns";
+import parse from "date-fns/parse";
+import { rowsMetaStateInitializer } from "@mui/x-data-grid/internals";
 
 class Calendar extends React.Component{
 
@@ -37,23 +45,72 @@ class Calendar extends React.Component{
     }
 
     renderDays(){
+      const dateType = "EEEE"
+      const days = []
+
+      let start = startOfWeek(this.state.currentMonth)
+
+      for(let i=0; i<7; i++){
+        days.push(
+          <div className="col col-center" key={i}>
+            {format(addDays(start, i), dateType)}
+          </div>
+        )
+      }
+
       return(
-        <div>
-          Days 1
+        <div className="days row">
+          {days}
         </div>
       )
     }
 
     renderCells(){
+      const {currentMonth, selectedDate} = this.state
+      const mStart = startOfMonth(currentMonth)
+      const mEnd = endOfMonth(mStart)
+      const startDate = startOfWeek(mStart)
+      const endDate = endOfWeek(mEnd)
+
+      const dateFormat = "d"
+      const cells = []
+
+      let days = []
+      let currentDay = startDate
+      let formattedDate = ""
+
+      while(currentDay <= endDate){
+        for(let i=0;i<7;i++){
+          formattedDate = format(currentDay, dateFormat)
+          const temp = currentDay
+          days.push(
+            <div className={`col cell ${!isSameMonth(temp,mStart)? "disabled":isSameDay(currentDay,selectedDate)? "selected":""}`} onClick={()=>this.onDateClick(currentDay)}>
+              <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+            </div>
+          )
+          currentDay = addDays(currentDay, 1)
+        }
+        cells.push(
+          <div className="row">
+              {days}
+          </div>
+        )
+        days = []
+      }
+
       return(
-        <div>
-          Cells 1
+        <div className="body">
+          {cells}
         </div>
       )
     }
 
     onDateClick = (date:Date) =>{
-
+      this.setState({
+        selectedDate: date
+      })
+      console.log(date)
     }
 
     nextMonth = () =>{
@@ -70,7 +127,7 @@ class Calendar extends React.Component{
 
     render() {
         return (
-          <div>
+          <div className="calendar">
             {this.renderHeader()}
             {this.renderDays()}
             {this.renderCells()}
