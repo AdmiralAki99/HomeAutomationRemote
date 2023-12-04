e4fbff59de6d7fba557034e27070b4b0d630f4e2
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-12-04 19:24:50 +0530

    Initializing State For Date Picker
    Initializing date picker state to store todays date as the default state

--- smart-remote/src/components/DatePicker.tsx
+++ smart-remote/src/components/DatePicker.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
+interface DatePickerComponentState{
+    selectedDate: Date,
+    open: boolean
+}
+
 class DatePicker extends React.Component{
-    
+
+    state: DatePickerComponentState = {
+        selectedDate: new Date(),
+        open: false
+    }
+
+    render(){
+
+        return(
+            <div>
+                Date Picker
+            </div>
+        )
+        
+    }
+
 }
