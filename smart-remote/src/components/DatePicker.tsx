ad61a98da34689549da5e01fd4cce05cd49af694
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-12-06 20:20:47 +0530

    Added DatePicker Styling and State Management
    Added date picker styling and state management

--- smart-remote/src/components/DatePicker.tsx
+++ smart-remote/src/components/DatePicker.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ReactDOM } from "react";
+import './DatePicker.css'
 
 interface DatePickerComponentState{
     selectedDate: Date,
@@ -7,19 +9,68 @@ interface DatePickerComponentState{
 
 class DatePicker extends React.Component{
 
-    state: DatePickerComponentState = {
+    state = {
         selectedDate: new Date(),
         open: false
+    }
+
+    constructor(props:any){
+        super(props)
+        this.state = {
+            selectedDate: new Date(),
+            open: false
+        }
+        this.createPopup = this.createPopup.bind(this)
+    }
+
+    // Create a constructor for the class
+
+    createPopup(){
+        console.log(this.state.open)
+    }
+
+    showDatePicker(displayStatus: boolean){
+        this.setState({
+            open: displayStatus
+        })
+    }
+
+    componentWillUnmount(): void {
+        
+    }
+
+    componentDidMount(): void {
+        this.setState({
+            selectedDate: new Date(),
+            open: false
+        })
+    }
+
+    componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{}>, snapshot?: any): void {
+        
     }
 
     render(){
 
         return(
-            <div>
-                Date Picker
+            <div className="date-picker">
+               <div className="date-picker-input" onClick={this.createPopup}>
+                    <input type="date"/>
+               </div>
+               {
+                this.state.open? (
+                    <div>
+                        <div className="date-picker-container">
+
+                        </div>
+                    </div>
+                ): ''
+               }
             </div>
         )
         
     }
 
 }
+
+export default DatePicker;
