fa27550ade9358f34aa485217b273057082dd032
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-12-07 23:09:15 +0530

    Added Date Grid For Date Picker
    Added date grid for date picker, need to add css styling

--- smart-remote/src/components/DatePicker.tsx
+++ smart-remote/src/components/DatePicker.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { ReactDOM } from "react";
 import './DatePicker.css'
 
+import { startOfMonth } from "date-fns";
+import startOfWeek from "date-fns/startOfWeek";
+import endOfWeek from "date-fns/endOfWeek";
+import endOfMonth from "date-fns/endOfMonth";
+import addDays from "date-fns/addDays";
+import format from "date-fns/format"
+import addMonths from "date-fns/addMonths";
+import isSameMonth from "date-fns/isSameMonth";
+import isSameDay from "date-fns/isSameDay";
+
 interface DatePickerComponentState{
     selectedDate: Date,
     open: boolean
@@ -35,6 +45,64 @@ class DatePicker extends React.Component{
         }else{
             this.showDatePicker(true)
         }
+    }
+
+    renderCells(){
+        const selectedDate = this.state.selectedDate
+
+        const mStart = startOfMonth(selectedDate.getMonth())
+        const mEnd = endOfMonth(selectedDate.getMonth())
+
+        return(
+            <div className="date-picker-days">
+                {this.renderDates()}
+            </div>
+        )
+
+    }
+
+    renderDates(){
+        const selectedDate = this.state.selectedDate
+
+        const mStart = startOfMonth(selectedDate.getMonth()-1)
+        const mEnd = endOfMonth(selectedDate.getMonth()-1)
+
+        const startDate = startOfWeek(mStart)
+        const endDate = endOfWeek(mEnd)
+
+        const rows = []
+
+        let days = []
+        let day = startDate
+        let formattedDate = ""
+
+        while(day <= endDate){
+            for(let i=0; i<7; i++){
+                formattedDate = format(day, "d")
+                const cloneDay = day
+                days.push(
+                    <div
+                        className={`col cell ${
+                            !isSameMonth(day, mStart)
+                            ? "disabled"
+                            : isSameDay(day, selectedDate) ? "selected" : ""
+                        }`}
+                        key={day.toString()}
+                        onClick={() => {console.log("Clicked on: ", cloneDay)}}
+                    >
+                        <span className="number">{formattedDate}</span>
+                    </div>
+                )
+                day = addDays(day, 1)
+            }
+            rows.push(
+                <div className="row" key={day.toString()}>
+                    {days}
+                </div>
+            )
+            days = []
+        }
+        return <div className="body">{rows}</div>
     }
 
     showDatePicker(displayStatus: boolean){
@@ -73,7 +141,7 @@ class DatePicker extends React.Component{
                                 Date Header
                             </div>
                             <div>
-                                Date Body
+                                {this.renderCells()}
                             </div>
                         </div>
                     </div>
