7966b4a6b44e78ab789cba2089f83c9b1adf3b0a
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2023-12-11 22:26:32 +0530

    Added Dynamic Page Creation Based On Server Database
    Added dynamic page creation based on server records

--- smart-remote/src/screens/NetworkScreen.tsx
+++ smart-remote/src/screens/NetworkScreen.tsx
@@ -1,5 +1,6 @@
 import {View} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useEffect, useState } from 'react';
 
 import { NetworkScreenNavBar, ScreenNavbar } from '../components/Navbar';
 import { ScreenParamList } from '../App';
@@ -9,15 +10,31 @@ import NetworkCard from '../components/NetworkCard'
 type NetworkProps = NativeStackScreenProps<ScreenParamList, 'Network'>;
 
 function NetworkScreen({route, navigation} : NetworkProps){
+    const [networkDevices, setNetworkDevices] = useState([])
+
+    const isEmpty = (val: any) => val == null || !(Object.keys(val) || val).length;
+
     navigation.navigate('Network')
+
+    const getNetworkDevices = () =>{
+        fetch('/network/get/all',{method:'GET'}).then(response => response.json()).then(
+            data => {
+                setNetworkDevices(data.devices)
+            }
+        )
+    }
+
+    useEffect(()=>{
+        getNetworkDevices()
+    })
 
     return(
         <View>
             <NetworkScreenNavBar navigation={navigation} destination={"Home"} />
-            <div className='grid grid-cols-2 justify-between pl-7'>
-                <NetworkCard/>
-                <NetworkCard/>
-                <NetworkCard/>
+            <div className='grid grid-cols-2 justify-between pl-7 pt-4 bg-red-400 w-screen max-w-screen-md h-screen max-h-screen'>
+                {isEmpty(networkDevices) ? <div></div> : networkDevices.map((device: any) => {
+                    return <NetworkCard deviceName={device.name} ipAddress={device.ip} macAddress={device.mac} wakeStatus={device.status}/>
+                })}
             </div>
         </View>
     )
