e6ce27fd9d01888dc7ed01add4f81ffa2a245499
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-11 15:06:32 -0500

    Added Elements To Display Video Stream On Frontend
    Added elements to display stream from server on the front end, need to figure out a faster method to process the stream

--- smart-remote/src/screens/CameraScreen.tsx
+++ smart-remote/src/screens/CameraScreen.tsx
@@ -3,17 +3,40 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { ScreenNavbar } from '../components/Navbar';
 import { ScreenParamList } from '../App';
+import { useEffect, useRef} from 'react';
 
 type CameraProps = NativeStackScreenProps<ScreenParamList, 'Camera'>;
 
 function CameraScreen({route, navigation} : CameraProps){
     navigation.navigate('Camera')
 
-    return(
-        <View>
-            <ScreenNavbar navigation={navigation} destination={"Home"} />
-        </View>
-    )
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    const getVideoFeed = async () => {
+        const resp = await fetch('/camera/get/feed')
+        const blob = await resp.blob()
+        
+        if(videoRef.current){
+            videoRef.current.src = URL.createObjectURL(blob)
+        }
+    }
+
+    useEffect(() => {
+      console.log("Getting Video Feed")
+      getVideoFeed()
+    },[])
+
+    return (
+      <View>
+        <ScreenNavbar navigation={navigation} destination={"Home"} />
+        <div>
+          <div className='p-6'>
+            <video ref={videoRef} width="640" height="480"></video>
+            {/* <img ref={videoRef} alt="Webcam Frame" width="640" height="640" /> */}
+          </div>
+        </div>
+      </View>
+    );
 
 }
 
