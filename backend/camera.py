70d56bf6dbfc954c380a7b3f9b41a40450ef9261
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-10 20:27:52 -0500

    Added Test Functions To Check Camera Footage On Localhost
    Added test functions to check camera footage, and send it on a test server on the local host, need to add a method to retrieve video feed from different cameras on IP

--- backend/camera.py
+++ backend/camera.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,jsonify
 import cv2
 
 class Camera:
     feed = cv2.VideoCapture(0)
     
     def __init__(self) -> None:
-        pass
+        ...
 
     def get_camera_feed(self) -> Response:
         while True:
             ret,frame = self.feed.read()
+
+            frame = cv2.resize(frame,(640,640))
 
             if not ret and frame is None:
                 continue
@@ -18,3 +20,18 @@ class Camera:
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    camera = Camera()
+
+    @app.route('/')
+    def index():
+        return jsonify({"message":"Hello World!"})
+    
+    @app.route('/camera')
+    def camera_feed():
+        return Response(camera.get_camera_feed(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+    app.run(debug=True)
