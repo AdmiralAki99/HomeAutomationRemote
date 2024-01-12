18a8cb3f5fb93c08f6f65d77a2a291e4bc531b80
AdmiralAki99 <akilesshw@gmail.com>
2024-01-12 16:50:50 -0500

    Created New Camera Feed Player Component
    Created new camera feed player component in order to allow for multiple cameras to be viewed

---
+++ smart-remote/src/components/CameraFeedPlayer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface CameraFeedPlayerState{
+    url : string
+}
+
+interface CameraFeedPlayerProps{
+    url : string
+}
+
+class CameraFeedPlayer extends React.Component{
+
+    state : CameraFeedPlayerState = {
+        url : ''
+    }
+
+    // Change the Properties To A Interface and remove ambiguous data types
+    constructor(props:CameraFeedPlayerProps){
+        super(props)
+        this.state = {
+            url : props.url
+        }
+
+
+    }
+
+    renderFeed(){
+        return(
+            <div>
+                <img src={this.state.url} />
+            </div>
+        )
+    }
+
+    render(){
+        return(
+            <div>
+                {this.renderFeed()}
+            </div>
+        )
+    }
+}
+
+export default CameraFeedPlayer;
