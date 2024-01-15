806aee5af23bfa90f51a2b29d5fd8e09ef2180f3
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-15 18:51:48 -0500

    Created Light Object
    Added Light object that will be responsible for interactions with a singular light bulb

---
+++ backend/lights.py
@@ -0,0 +1,59 @@
+import asyncio
+from kasa import SmartBulb,Discover
+
+class Light:
+
+    bulb = None
+
+    def __init__(self, ip):
+        self.ip = ip
+        self.bulb = SmartBulb(ip)
+
+    def get_state(self):
+        if self.bulb is None:
+            return None
+
+        if self.bulb.is_on:
+            return "on"
+        else:
+            return "off"
+        
+    def get_alias(self):
+        if self.bulb is None:
+            return None
+
+        return self.bulb.alias
+    
+    def get_ip(self):
+        return self.ip
+    
+    def turn_on(self):
+        if self.bulb is None:
+            return None
+
+        asyncio.run(self.bulb.turn_on())
+        if self.bulb.is_on:
+            return True
+        else:
+            return False
+        
+    def turn_off(self):
+        if self.bulb is None:
+            return None
+
+        asyncio.run(self.bulb.turn_off())
+        if self.bulb.is_off:
+            return True
+        else:
+            return False
+    
+
+
+
+if __name__ == "__main__":
+    bulb = SmartBulb("10.0.0.167")
+    asyncio.run(bulb.update())
+    if bulb.is_on:
+        asyncio.run(bulb.turn_off())
+    else:
+        asyncio.run(bulb.turn_on())
