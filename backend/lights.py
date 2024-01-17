7c64aebc653adae03e51a41ebee9b1edc60ff636
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-17 17:39:51 -0500

    Added More Light Attributes
    Added more light attributes to make properties to be manipulated easily and stored in the databases better. Device class is used as a wrapper to discover multiple devices and store the data in the database

--- backend/lights.py
+++ backend/lights.py
@@ -4,10 +4,17 @@ from kasa import SmartBulb,Discover
 class Light:
 
     bulb = None
+    ip = None
+    state = None
+    color = None
+    brightness = None
+    name = None
 
     def __init__(self, ip):
         self.ip = ip
         self.bulb = SmartBulb(ip)
+        self.brightness = self.bulb.brightness
+        self.name = self.bulb.alias
 
     def get_state(self):
         if self.bulb is None:
@@ -47,6 +54,28 @@ class Light:
         else:
             return False
         
+    def get_brightness(self):
+        return self.brightness
+    
+    def set_brightness(self, brightness):
+        if self.bulb is None:
+            return None
+
+        asyncio.run(self.bulb.set_brightness(brightness))
+        self.brightness = brightness
+        
+    def get_alias(self):
+        if self.bulb is None:
+            return None
+
+        return self.name
+    
+    def set_alias(self, name):
+        if self.bulb is None:
+            return None
+
+        asyncio.run(self.bulb.set_alias(name))
+        self.name = name
 
 
     class Device:
@@ -58,7 +87,12 @@ class Light:
         def scan(self):
             discovery = asyncio.run(Discover.discover())
             return discovery
-    
+        
+        def add_light(self, ip,name):
+            self.lights.append(Light(ip))
+
+        def get_lights(self):
+            return {"lights": self.lights}
 
 
 
