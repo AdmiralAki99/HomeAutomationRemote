fbde2ac9b8589e16f6670571dae05d2c51b79306
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-19 00:03:20 -0500

    Added Modal To Camera Card
    Added modal to test if it can be used to display camera feed

--- smart-remote/src/components/CameraCard.tsx
+++ smart-remote/src/components/CameraCard.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import CameraIcon from '@mui/icons-material/Camera';
 import InfoIcon from '@mui/icons-material/Info';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import { Button } from "@mui/material";
+import CameraFeedPlayer from "./CameraFeedPlayer";
 
 interface CameraCardProps{
     cameraName: string;
     cameraUrl: string;
 }
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    height: 600,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
+
 class CameraCard extends React.Component<CameraCardProps>{
 
     state : CameraCardProps = {
         cameraName: '',
-        cameraUrl: ''
+        cameraUrl: '',
     }
+
+    modalOpen = false
 
     constructor(props:CameraCardProps){
         super(props)
@@ -22,6 +43,9 @@ class CameraCard extends React.Component<CameraCardProps>{
             cameraName: this.props.cameraName,
             cameraUrl: this.props.cameraUrl
         }
+        this.handleOpenModal = this.handleOpenModal.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this)
+        this.renderCameraFeed = this.renderCameraFeed.bind(this)
     }
 
     componentDidMount(): void {
@@ -29,7 +53,20 @@ class CameraCard extends React.Component<CameraCardProps>{
     }
 
     componentDidUpdate(prevProps: Readonly<CameraCardProps>, prevState: Readonly<{}>, snapshot?: any): void {
-        
+        console.log(this.modalOpen)
+    }
+
+    handleCloseModal = () => {
+        console.log("Closing Modal")
+        console.log(this.modalOpen)
+        this.modalOpen = false
+        console.log(this.modalOpen)
+    }
+
+    handleOpenModal(){
+        console.log("Opening Modal")
+        this.modalOpen = true
+        console.log(this.modalOpen)
     }
 
     renderTitle(){
@@ -40,14 +77,39 @@ class CameraCard extends React.Component<CameraCardProps>{
         )
     }
 
-    renderActionButtons(){
+    renderCameraFeed(){
         return(
-            <div className="grid grid-rows-3 gap-4 justify-center">
-                <button className="text-white"><CameraIcon sx={{color:'white'}}></CameraIcon></button>
-                <button><InfoIcon sx={{color:"white"}}></InfoIcon></button>
-                <button><PowerSettingsNewIcon sx={{color:"white"}}></PowerSettingsNewIcon></button>
-            </div>
+            <Modal
+            open={this.modalOpen}
+            onClose={this.handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <div className="flex justify-end items-end">
+                <Button onClick={this.handleCloseModal}>Close</Button>
+              </div>
+            </Box>
+          </Modal>
         )
+    }
+
+    renderActionButtons(){
+        return (
+          <div className="grid grid-rows-3 gap-4 justify-center">
+            <button className="text-white" onClick={this.handleOpenModal}>
+              <CameraIcon sx={{ color: "white" }}></CameraIcon>
+            </button>
+            <button>
+              <InfoIcon sx={{ color: "white" }}></InfoIcon>
+            </button>
+            <button>
+              <PowerSettingsNewIcon
+                sx={{ color: "white" }}
+              ></PowerSettingsNewIcon>
+            </button>
+          </div>
+        );
     }
 
     render(){
@@ -59,6 +121,7 @@ class CameraCard extends React.Component<CameraCardProps>{
                 <div>{this.renderActionButtons()}</div>
               </div>
             </a>
+            {(this.modalOpen)?<CameraFeedPlayer url="/link"></CameraFeedPlayer>:null}
           </div>
         );
     }
