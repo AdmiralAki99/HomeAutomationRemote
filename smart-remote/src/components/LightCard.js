efa088e6b1325c8858dcaf354a4e4b18b4bc0f99
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-24 21:37:28 -0500

    Added Modal To Light Cards
    Added modal to light cards to allow for information displaying and changing stuff that can be then stored on the backend using post requests.

--- smart-remote/src/components/LightCard.js
+++ smart-remote/src/components/LightCard.js
@@ -24,7 +24,7 @@ const LightCard = ({min,value,label,onChange,isChecked,onCheck}) => {
           id={"checkbox-1"}
           onChange={onCheck}
         />
-        <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
+        <a className="block max-w-lg w-80 h-40 p-6 bg-black border border-gray-200 rounded-md shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70">
           <div className="grid grid-rows-3 grid-cols-2 gap-4">
             <div className="row row-start-1 row-end-3 text-white">{label}</div>
             <div className="flex row-start-2 row-end-4 items-center justify-center h-full w-full object-fill pt-6 pl-6">
@@ -36,6 +36,27 @@ const LightCard = ({min,value,label,onChange,isChecked,onCheck}) => {
             </div>
           </div>
         </a>
+
+        {/* <Modal
+          open={manageModalOpen}
+          onClose={handleCloseManageModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className="flex justify-end items-end">
+              <Button onClick={handleCloseManageModal}>Close</Button>
+              <Button onClick={handleScanList}>Scan</Button>
+              <Button>Connect</Button>
+            </div>
+            <div>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Light Name
+              </Typography>
+              <TextField id="outlined-basic" variant="outlined" />
+            </div>
+          </Box>
+        </Modal> */}
       </>
     );
 
