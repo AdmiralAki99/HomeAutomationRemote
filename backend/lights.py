af0411b546168e9a57f9912d68b7b22245ffb53a
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-01-30 23:36:00 -0500

    Added Some Functions For Simpler Management Of AsyncIO & Standalone Test
    Added more functions to handle async io in the backend and added standalone test to check if the new method works.

--- backend/lights.py
+++ backend/lights.py
@@ -1,5 +1,7 @@
 import asyncio
-from kasa import SmartBulb,Discover
+from kasa import SmartBulb,Discover,Credentials
+from dotenv import load_dotenv
+import os
 
 class Light:
 
@@ -12,8 +14,11 @@ class Light:
 
     def __init__(self, ip):
         self.ip = ip
-        self.bulb = SmartBulb(ip)
-        asyncio.run(self.bulb.update())
+        asyncio.run(self.__init_bulb__())
+
+    async def __init_bulb__(self):
+        self.bulb = SmartBulb(self.ip)
+        await self.bulb.update()
         self.brightness = self.bulb.brightness
         self.name = self.bulb.alias
 
@@ -35,34 +40,23 @@ class Light:
     def get_ip(self):
         return self.ip
     
-    def turn_on(self):
-        if self.bulb is None:
-            return None
-
-        asyncio.run(self.bulb.turn_on())
-        if self.bulb.is_on:
-            return True
-        else:
-            return False
+    async def turn_on(self):
+        await self.bulb.turn_on()
         
-    def turn_off(self):
-        if self.bulb is None:
-            return None
+    async def turn_off(self):
+        await self.bulb.turn_off()
+        await self.bulb.update()
 
-        asyncio.run(self.bulb.turn_off())
-        if self.bulb.is_off:
-            return True
-        else:
-            return False
         
     def get_brightness(self):
         return self.brightness
     
-    def set_brightness(self, brightness):
+    async def set_brightness(self, brightness):
         if self.bulb is None:
             return None
 
-        asyncio.run(self.bulb.set_brightness(brightness))
+        await self.bulb.set_brightness(brightness)
+        await self.bulb.update()
         self.brightness = brightness
         
     def get_alias(self):
@@ -71,44 +65,48 @@ class Light:
 
         return self.name
     
-    def set_alias(self, name):
+    async def set_alias(self, name):
         if self.bulb is None:
             return None
 
-        asyncio.run(self.bulb.set_alias(name))
+        await self.bulb.set_alias(name)
+        await self.bulb.update()
+        
         self.name = name
 
     def get_json_data(self):
         return {"ip": self.ip, "name": self.name, "state": self.get_state()}
 
-
-    class Device:
-        lights = []
-
-        def __init__(self):
-            ...
-
-        def scan(self):
-            discovery = asyncio.run(Discover.discover())
-            return discovery
-        
-        def add_light(self, ip,name):
-            self.lights.append(Light(ip))
-
-        def get_lights(self):
-            return {"lights": self.lights}
-        
-
 """
 {'10.0.0.167': <DeviceType.Bulb model KL125(US) at 10.0.0.167 (Desk Lamp), is_on: True - dev specific: {'Brightness': 9, 'Is dimmable': True, 'Color temperature': 4150, 'Valid temperature range': ColorTempRange(min=2500, max=6500), 'HSV': HSV(hue=0, saturation=0, value=9), 'Presets': [SmartBulbPreset(index=0, brightness=50, hue=0, saturation=0, color_temp=2700, custom=None, id=None, mode=None), SmartBulbPreset(index=1, brightness=100, hue=0, saturation=100, color_temp=0, custom=None, id=None, mode=None), SmartBulbPreset(index=2, brightness=100, hue=120, saturation=100, color_temp=0, custom=None, id=None, mode=None), SmartBulbPreset(index=3, brightness=100, hue=240, saturation=100, color_temp=0, custom=None, id=None, mode=None)]}>}
 """
 
+async def discover_devices():
+    load_dotenv()
+    devices = await Discover.discover(
+        credentials= Credentials(f'{os.getenv("KASA_USERNAME")}', f'{os.getenv("KASA_PASSWORD")}'),
+        timeout=20,
+    )
 
+    lights= []
+    
+    for ip,device in devices.items():
+        await device.update()
+        lights.append({"ip": ip, "name": device.alias, "state": device.is_on})
+
+    return lights
+
+    
 
 if __name__ == "__main__":
-    bulb = SmartBulb("10.0.0.167")
-    asyncio.run(bulb.update())
-    if bulb.is_on:
-        asyncio.run(bulb.turn_off())
-    else:
-        asyncio.run(bulb.turn_on())
+
+    # bulb = SmartBulb("10.0.0.167")
+    # asyncio.run(bulb.update())
+    # if bulb.is_on:
+    #     asyncio.run(bulb.turn_off())
+    # else:
+    #     asyncio.run(bulb.turn_on())
+
+    bulb = Light("10.0.0.167")
+    print(bulb.get_json_data())
+    print(asyncio.run(bulb.turn_off()))
