78abfcef91fb944f91117c2f0ae66347500cd35a
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-02-12 00:21:12 -0500

    Added Initial URL's & Scraped Search Page
    Added initial url and scraped search page initially before creating json values and linking to backend and routes

--- backend/GutenbergScraper.py
+++ backend/GutenbergScraper.py
@@ -6,6 +6,9 @@ class GutenbergAPI:
 
     __url__ = {
         'base_ebooks': 'https://www.gutenberg.org/ebooks/',
+        'base_search': 'https://www.gutenberg.org/ebooks/search/?query=',
+        'base_download': 'https://www.gutenberg.org/ebooks/',
+        'image': 'https://www.gutenberg.org/',
     }
 
 
@@ -14,7 +17,13 @@ class GutenbergAPI:
         pass
 
     def search_book(self, book_title: str) -> str:
-        pass
+        resp = requests.get(f"{self.__url__['base_search']}{book_title}")
+
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.content, "html.parser")
+            for tags in soup.find_all("li",class_="booklink"):
+                print(f"Title: {tags.find('span',class_='title').text}")
+                print("Image: ",tags.find('img',class_='cover-thumb'))
 
     def get_searched_book_metadata(self, book_title: str) -> str:
         pass
@@ -27,12 +36,6 @@ class GutenbergAPI:
 
 
 if __name__ == "__main__":
-    resp = requests.get("https://www.gutenberg.org/ebooks/1342")
-
-    if resp.status_code == 200:
-        soup = BeautifulSoup(resp.content, "html.parser")
-        
-        for tags in soup.find_all("a",class_="link"):
-            print(tags.get('href'))
-
+    g = GutenbergAPI()
+    g.search_book("art of war")
 
