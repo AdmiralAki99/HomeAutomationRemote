1af1a0c515d308d0f441c18fce86d4192a4a424a
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-02-13 23:55:59 -0500

    Added Refresh Token Method Call
    Successfully stored and able to refresh token. Need to add functions to utilize API

--- backend/mangaManager.py
+++ backend/mangaManager.py
@@ -6,6 +6,11 @@ import os
 
 class MangaManager:
 
+    access_token = None
+    refresh_token = None
+    expires_in = None
+    refresh_expires_in = None
+
     __url__ = {
         'base_url':'https://api.mangadex.org',
         'auth_url':'https://auth.mangadex.org/realms/mangadex/protocol/openid-connect/token',
@@ -35,12 +40,40 @@ class MangaManager:
         resp = requests.post(self.__url__['auth_url'], data=query,headers=headers)
 
         if resp.status_code == 200:
-            return resp.json()
+            self.access_token = resp.json()['access_token']
+            self.refresh_token = resp.json()['refresh_token']
+            self.expires_in = resp.json()['expires_in']
+            self.refresh_expires_in = resp.json()['refresh_expires_in']
+            return "Authenticated"
         else:
             return resp.text
+        
+
+    def token_refresh(self) -> str:
+        headers = self.get_headers()
+
+        query = {
+            'grant_type':'refresh_token',
+            'refresh_token': self.refresh_token,
+            'client_id': os.getenv('MANGADEX_API_PERSONAL_ClIENT_ID'),
+            'client_secret': os.getenv('MANGADEX_API_PERSONAL_SECRET')
+        }
+
+        resp = requests.post(self.__url__['auth_url'], data=query,headers=headers)
+
+        if resp.status_code == 200:
+            self.access_token = resp.json()['access_token']
+            self.refresh_token = resp.json()['refresh_token']
+            self.expires_in = resp.json()['expires_in']
+            self.refresh_expires_in = resp.json()['refresh_expires_in']
+            return "Token Refreshed"
+        else:
+            return resp.text
+
         
 
 if __name__ == "__main__":
     m = MangaManager()
     print(m.authenticate())
+    print(m.token_refresh())
 
