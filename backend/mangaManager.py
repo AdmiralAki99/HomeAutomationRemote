fcc4ed265ca6c5b479a1010c89503ff99e2284b8
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-02-17 01:44:48 -0500

    Created New Manga Class To Encapsulate Functionality
    Added Manga class to encapsulate functions and attributes in the future. Also added checks for the api since it is not consistent and contains many errors in json objects from server side and has many missing keys and values

--- backend/mangaManager.py
+++ backend/mangaManager.py
@@ -1,8 +1,28 @@
 import requests
 import dotenv
 import os
+from datetime import datetime
+import json
 
+class Manga:
 
+    def __init__(self) -> None:
+        pass
+
+    def __init__(self,id,title,alt_title,description,publication_demographic,status,tags,cover_art,artist,author) -> None:
+        self.id = id
+        self.title = title
+        self.alt_title = alt_title
+        self.description = description
+        self.publication_demographic = publication_demographic
+        self.status = status
+        self.tags = tags
+        self.cover_art = cover_art
+        self.artist = artist
+        self.author = author
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}\nAlt Title: {self.alt_title}\nDescription: {self.description}\nPublication Demographic: {self.publication_demographic}\nStatus: {self.status}\nTags: {self.tags}\nCover Art: {self.cover_art}\nArtist: {self.artist}\nAuthor: {self.author}"
 
 class MangaManager:
 
@@ -10,6 +30,8 @@ class MangaManager:
     refresh_token = None
     expires_in = None
     refresh_expires_in = None
+
+    manga_keys = ['id','title','altTitles','description','publicationDemographic','status','tags','coverArt','artist','author']
 
     __url__ = {
         'base_url':'https://api.mangadex.org',
@@ -24,6 +46,12 @@ class MangaManager:
         return {
             'Content-Type':'application/x-www-form-urlencoded',
         }
+    
+    def verify_token(self) -> bool:
+        if self.expires_in > datetime.now().timestamp():
+            return True
+        else:
+            return False
 
     def authenticate(self) -> str:
 
@@ -42,7 +70,7 @@ class MangaManager:
         if resp.status_code == 200:
             self.access_token = resp.json()['access_token']
             self.refresh_token = resp.json()['refresh_token']
-            self.expires_in = resp.json()['expires_in']
+            self.expires_in = datetime.now().timestamp() + resp.json()['expires_in']
             self.refresh_expires_in = resp.json()['refresh_expires_in']
             return "Authenticated"
         else:
@@ -64,16 +92,31 @@ class MangaManager:
         if resp.status_code == 200:
             self.access_token = resp.json()['access_token']
             self.refresh_token = resp.json()['refresh_token']
-            self.expires_in = resp.json()['expires_in']
+            self.expires_in = datetime.now().timestamp() + resp.json()['expires_in']
             self.refresh_expires_in = resp.json()['refresh_expires_in']
             return "Token Refreshed"
         else:
             return resp.text
+        
+    def search(self,title:str):
+        if self.verify_token() == False:
+            self.authenticate()
+        headers = self.get_headers()
+
+        query = {
+            'title':title
+        }
+
+        resp = requests.get(f'{self.__url__["base_url"]}/manga',params=query,headers=headers)
+
+        # if resp.status_code == 200:
+        #     for values in resp.json()['data']:
+        #        # Check if all keys exist in manga_keys
+               
 
         
 
 if __name__ == "__main__":
     m = MangaManager()
     print(m.authenticate())
-    print(m.token_refresh())
-
+    print(m.search("one piece"))
