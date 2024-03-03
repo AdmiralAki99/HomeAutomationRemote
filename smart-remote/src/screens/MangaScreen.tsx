fbb2d1b7cd8cec6a06b1013bdd7517d1b690f950
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-03-03 02:20:10 -0500

    Added More UI Elements To Show Multiple Manga Genres
    Added more UI elements to show to show a new row of randomized category

--- smart-remote/src/screens/MangaScreen.tsx
+++ smart-remote/src/screens/MangaScreen.tsx
@@ -18,6 +18,12 @@ type mangaProps = NativeStackScreenProps<ScreenParamList, 'Manga'>;
 function MangaScreen({route, navigation} : mangaProps){
 
   const [mangaFeed, setMangaFeed] = useState<any>([]);
+  const [randomGenre, setRandomGenre] = useState<any>([]);
+
+  useEffect(()=>{
+    getMangaFeed();
+    getRandomGenre();
+  },[])
 
   const getMangaFeed = async () => {
     try{
@@ -28,7 +34,17 @@ function MangaScreen({route, navigation} : mangaProps){
       console.log('Error fetching manga feed')
     }
   }
+
+  const getRandomGenre = async () => {
+    try{
+      const resp = await axios.get('/manga/genre/random')
+      setRandomGenre(resp.data);
+    }
+    catch(e){
+      console.log('Error fetching random genre')
+    }
   
+  }
 
   return (
     <View>
@@ -48,8 +64,23 @@ function MangaScreen({route, navigation} : mangaProps){
                       );
                     })
                   }
-                
-                
+              </div>
+          </div>
+        </div>
+        <div className='flex flex-row bg-noir m-auto pt-10'>
+          <div className='flex overflow-x-scroll pb-10 hide-scroll-bar'>
+              <div className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10'>
+                 {
+                    randomGenre.map((manga:any)=>{
+                      return (
+                        <div className="inline-block px-3">
+                          <div className="w-40 h-60 max-w-xs overflow-hidden rounded-lg shadow-md bg-bubblegum hover:shadow-xl transition-none duration-300 ease-in-out shadow-white">
+                            <img src={manga.manga.coverArt} className='w-40 h-60'></img>
+                          </div>
+                        </div>
+                      );
+                    })
+                  }
               </div>
           </div>
         </div>
