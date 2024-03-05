c5d2d01305c032e5fffe523e1ccf09a30452e2f7
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-03-04 23:36:26 -0500

    Refactored Screen As Component To Call GET Requests At Mount
    Refactored screen as a component to call GET functions from the backend for multiple carousels.

--- smart-remote/src/screens/MangaScreen.tsx
+++ smart-remote/src/screens/MangaScreen.tsx
@@ -15,46 +15,69 @@ import MangaReader from '../components/MangaReader';
 
 type mangaProps = NativeStackScreenProps<ScreenParamList, 'Manga'>;
 
-function MangaScreen({route, navigation} : mangaProps){
+interface GenreRow{
+  genre: string;
+  manga: any[];
+}
 
-  const [mangaFeed, setMangaFeed] = useState<any>([]);
-  const [randomGenre, setRandomGenre] = useState<any>([]);
+interface MangaScreenStateType {
+  mangaFeed: any[];
+  genre: GenreRow[];
+  randomGenres: any[];
+}
 
-  useEffect(()=>{
-    getMangaFeed();
-    getRandomGenre();
-  },[])
+class MangaScreen extends React.Component<mangaProps> {
+  state : MangaScreenStateType = {
+    mangaFeed: [],
+    randomGenres: [],
+    genre: [],
+  }
 
-  const getMangaFeed = async () => {
+  componentDidMount(): void {
+    this.getMangaFeed();
+    this.getRandomGenre();
+  }
+
+  constructor(props: any){
+    super(props);
+    this.getMangaFeed = this.getMangaFeed.bind(this);
+    this.getRandomGenre = this.getRandomGenre.bind(this);
+  }
+
+  getMangaFeed = async () => {
     try{
       const resp = await axios.post('/manga/search',{'title':''})
-      setMangaFeed(resp.data);
+      this.setState({mangaFeed: resp.data});
     }
     catch(e){
       console.log('Error fetching manga feed')
     }
   }
 
-  const getRandomGenre = async () => {
+  getRandomGenre = async () => {
     try{
-      const resp = await axios.get('/manga/genre/random')
-      setRandomGenre(resp.data);
-    }
+        await fetch('/manga/get/random',{method:'GET'}).then((resp)=>resp.json()).then((data)=>{
+          this.setState({randomGenres: data.manga})
+           }
+        );
+      }
     catch(e){
       console.log('Error fetching random genre')
     }
   
   }
 
-  return (
-    <View>
-      <ScreenNavbar navigation={navigation} destination={"Home"} />
-      <div>
+
+  render(){
+    return (
+      <View>
+        <ScreenNavbar navigation={this.props.navigation} destination={"Home"} />
+        <div>
         <div className='flex flex-row bg-noir m-auto pt-10'>
           <div className='flex overflow-x-scroll pb-10 hide-scroll-bar'>
               <div className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10'>
                  {
-                    mangaFeed.map((manga:any)=>{
+                    this.state.mangaFeed.map((manga:any)=>{
                       return (
                         <div className="inline-block px-3">
                           <div className="w-40 h-60 max-w-xs overflow-hidden rounded-lg shadow-md bg-bubblegum hover:shadow-xl transition-none duration-300 ease-in-out shadow-white">
@@ -71,11 +94,11 @@ function MangaScreen({route, navigation} : mangaProps){
           <div className='flex overflow-x-scroll pb-10 hide-scroll-bar'>
               <div className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10'>
                  {
-                    randomGenre.map((manga:any)=>{
+                    this.state.randomGenres.map((manga:any)=>{
                       return (
                         <div className="inline-block px-3">
                           <div className="w-40 h-60 max-w-xs overflow-hidden rounded-lg shadow-md bg-bubblegum hover:shadow-xl transition-none duration-300 ease-in-out shadow-white">
-                            <img src={manga.manga.coverArt} className='w-40 h-60'></img>
+                            <img src={manga.coverArt} className='w-40 h-60'></img>
                           </div>
                         </div>
                       );
@@ -84,10 +107,11 @@ function MangaScreen({route, navigation} : mangaProps){
               </div>
           </div>
         </div>
-        <button onClick={getMangaFeed}>Fetch</button>
+        {/* <button onClick={this.getRandomGenre}>Fetch</button> */}
       </div>
-    </View>
-  );
+      </View>
+    );
+  }
 }
 
 
