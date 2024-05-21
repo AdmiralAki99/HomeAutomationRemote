9baf8a35c4ad99a39d9d6806d9d38092ceef6b42
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-05-20 23:34:37 -0400

    Testing Keyboard
    Added UI/UX in order to test the keyboard. Need to hook the component so that the value is returned properly.

--- smart-remote/src/screens/TestScreen.tsx
+++ smart-remote/src/screens/TestScreen.tsx
@@ -2,9 +2,10 @@ import React ,{useState, useEffect} from 'react';
 import { NavigationContainer, useNavigation} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from '@mui/material';
-import {View} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { ScreenParamList } from '../App';
+import {Keyboard, Text, TextInput, StyleSheet, View} from 'react-native';
+import { KeyboardComponent } from '../components/KeyboardComponent';
 
 import { HomeScreenNavbar, LightScreenNavBar, ScreenNavbar } from '../components/Navbar';
 import LightCard from '../components/LightCard';
@@ -14,23 +15,42 @@ import { error } from 'console';
 
 type testProps = NativeStackScreenProps<ScreenParamList, 'Test'>;
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-  height: 600,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-};
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 36,
+  },
+  input: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 4,
+  },
+  status: {
+    padding: 10,
+    textAlign: 'center',
+  },
+});
 
 
 function TestScreen({route, navigation} : testProps){
     navigation.navigate('Test')
     const [data,setData] = useState<Record<string,any>>([])
     const [modalOpen, setOpen] = useState(false);
+    const [keyboardStatus, setKeyboardStatus] = useState('');
+
+    useEffect(() => {
+      const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+        setKeyboardStatus('Keyboard Shown');
+      });
+      const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
+        setKeyboardStatus('Keyboard Hidden');
+      });
+  
+      return () => {
+        showSubscription.remove();
+        hideSubscription.remove();
+      };
+    }, []);
 
   const handleOpen = () =>{
     setOpen(true)
@@ -42,37 +62,19 @@ function TestScreen({route, navigation} : testProps){
 
   const isEmpty = (val: any) => val == null || !(Object.keys(val) || val).length;
 
-  const updateLight = async () =>{
-    // await fetch("/light/3",{method:'PUT',body: JSON.stringify({state:"off"})}).then(
-    //   res => res.json()
-    // ).then(
-    //   data => {
-    //     // setData(data)
-    //     console.log(data)
-    //   }
-    // )
-    axios.put("/light/3",{state:"off"}).then(
-      response => console.log(response)
-    ).catch(error => console.log(error))
-  }
+  const handleLogin = async () => {
+    try {
+      const response = await axios.get('/spotify/login').catch((error) => {
+        console.error('Error during Spotify login:', error);
+      })
+    } catch (error) {
+      console.error('Error during Spotify login:', error);
+    }
+  };
 
     return (
       <View>
-        {/* <h1>Test Screen</h1>
-            <Button onClick={() => navigation.navigate('Light')}>
-                Light Screen
-            </Button> */}
-        <LightScreenNavBar navigation={navigation} destination={"Home"} />
-        <Button onClick={handleOpen}>Press This</Button>
-        {/* {isEmpty(data) === true ? (
-          <h1>Loading....</h1>
-        ) : (
-          data.map((light: any, index: any) => (
-            <div key={index}>
-              <h1>{light["name"]}</h1>
-            </div>
-          ))
-        )} */}
+        <KeyboardComponent/>
       </View>
     );
 
