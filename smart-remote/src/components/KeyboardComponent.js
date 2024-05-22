74ba29c7a14b09009a861cb599b98d008e5844ea
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-05-21 20:37:34 -0400

    Added Successful Keyboard Toggling & Hiding
    Added successful keyboard and toggling and hiding based on user touch on input labels and hide button on the keyboard

--- smart-remote/src/components/KeyboardComponent.js
+++ smart-remote/src/components/KeyboardComponent.js
@@ -2,10 +2,44 @@ import React from "react";
 import Keyboard from 'react-simple-keyboard'
 import "react-simple-keyboard/build/css/index.css";
 
+let layout = {
+  default: [
+    "q w e r t y u i o p",
+    "a s d f g h j k l",
+    "{shift} z x c v b n m {backspace}",
+    "{numbers} {space} {ent} {hide}"
+  ],
+  shift: [
+    "Q W E R T Y U I O P",
+    "A S D F G H J K L",
+    "{shift} Z X C V B N M {backspace}",
+    "{numbers} {space} {ent} {hide}"
+  ],
+  numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"]
+}
+let display = {
+  "{numbers}": "123",
+  "{space}": " ",
+  "{ent}": "return",
+  "{escape}": "esc ⎋",
+  "{tab}": "tab ⇥",
+  "{backspace}": "⌫",
+  "{capslock}": "caps lock ⇪",
+  "{shift}": "⇧",
+  "{controlleft}": "ctrl ⌃",
+  "{controlright}": "ctrl ⌃",
+  "{altleft}": "alt ⌥",
+  "{altright}": "alt ⌥",
+  "{metaleft}": "cmd ⌘",
+  "{metaright}": "cmd ⌘",
+  "{abc}": "ABC"
+}
+
 class KeyboardComponent extends React.Component{
   state = {
-    layoutName: "default",
+    layoutName: "shift",
     input: "",
+    isNumbers: false,
     onChange:()=>{},
     onKeyPress:()=>{}
   };
@@ -16,8 +50,12 @@ class KeyboardComponent extends React.Component{
       layoutName: props.layoutName,
       input: props.input,
       onChange: props.onChange,
-      onKeyPress: props.onKeyPress
+      onKeyPress: props.onKeyPress,
     }
+  }
+
+  onComponentDidUpdate = (prevProps) => {
+    console.log("Props updated", prevProps);
   }
 
   onKeyPress = button => {
@@ -27,7 +65,27 @@ class KeyboardComponent extends React.Component{
      * If you want to handle the shift and caps lock buttons
      */
     if (button === "{shift}" || button === "{lock}") this.handleShift();
+    if (button === '{numbers}' || button === "{abc}") this.handleNumbers()
+    if (button === "{hide}") this.props.onHide()
   };
+
+  handleNumbers = () =>{
+    if(this.state.layoutName !== "numbers"){
+      this.setState({
+        layoutName: "numbers"
+      });
+    }else{
+      this.setState({
+        layoutName: "default"
+      });
+    }
+  }
+
+  closeKeyboard = () => {
+    this.setState({
+      isToggled: false
+    })
+  }
 
   handleShift = () => {
     const layoutName = this.state.layoutName;
@@ -45,10 +103,12 @@ class KeyboardComponent extends React.Component{
 
   render() {
     return (
-      <div>
+      <div className={`keyboardContainer ${(this.props.isToggled === false)? "hidden":""}`}>
         <Keyboard
           keyboardRef={r => (this.keyboard = r)}
           layoutName={this.state.layoutName}
+          layout={layout}
+          display={display}
           onChange={this.state.onChange}
           onKeyPress={this.onKeyPress}
         />
