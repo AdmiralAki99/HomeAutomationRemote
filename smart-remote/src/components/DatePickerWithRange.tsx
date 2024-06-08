2b0248778e34ac80a427614cb5c4d07b14b5e720
AdmiralAki99 <47061767+AdmiralAki99@users.noreply.github.com>
2024-06-07 22:20:08 -0400

    Changed ShadCN Component
    Changed ShadCN component to my normal component template to allow for more manipulation and a better way to get the values from the date range picker.

--- smart-remote/src/components/DatePickerWithRange.tsx
+++ smart-remote/src/components/DatePickerWithRange.tsx
@@ -14,52 +14,139 @@ import {
   PopoverTrigger,
 } from "../@/components/ui/popover"
 
-export default function DatePickerWithRange({
-  className,
-}: React.HTMLAttributes<HTMLDivElement>) {
-  const [date, setDate] = React.useState<DateRange | undefined>({
-    from: new Date(),
-    to: addDays(new Date(), 20),
-  })
+interface DatePickerWithRangeProps{
+  className:string,
+  from:string,
+  to:string
 
-  return (
-    <div className={cn("grid gap-2 z-50", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
-    </div>
-  )
 }
+
+// export default function DatePickerWithRange({ className,from,to }: { className?: string ,from?:string,to?:string}){
+
+//   const [date, setDate] = (from && to) ? React.useState<DateRange>({
+//     from: new Date(from),
+//     to: new Date(to),
+//   }):React.useState<DateRange | undefined>({
+//     from: new Date(),
+//     to: addDays(new Date(), 20),
+//   })
+
+//   return (
+//     <div className={cn("grid gap-2 z-50", className)}>
+//       <Popover>
+//         <PopoverTrigger asChild>
+//           <Button
+//             id="date"
+//             variant={"outline"}
+//             className={cn(
+//               "w-[300px] justify-start text-left font-normal",
+//               !date && "text-muted-foreground"
+//             )}
+//           >
+//             <CalendarIcon className="mr-2 h-4 w-4" />
+//             {date?.from ? (
+//               date.to ? (
+//                 <>
+//                   {format(date.from, "LLL dd, y")} -{" "}
+//                   {format(date.to, "LLL dd, y")}
+//                 </>
+//               ) : (
+//                 format(date.from, "LLL dd, y")
+//               )
+//             ) : (
+//               <span>Pick a date</span>
+//             )}
+//           </Button>
+//         </PopoverTrigger>
+//         <PopoverContent className="w-auto p-0" align="start">
+//           <Calendar
+//             initialFocus
+//             mode="range"
+//             defaultMonth={date?.from}
+//             selected={date}
+//             onSelect={setDate}
+//             numberOfMonths={2}
+//           />
+//         </PopoverContent>
+//       </Popover>
+//     </div>
+//   )
+// }
+
+class DatePickerWithRange extends React.Component<DatePickerWithRangeProps> {
+
+  state = {
+    from: '',
+    to: '',
+    date: {
+      from: new Date(),
+      to: addDays(new Date(), 20),
+    }
+  }
+
+  constructor(props: DatePickerWithRangeProps) {
+    super(props);
+    this.state = {
+      from: (props.from) ? props.from : '',
+      to: (props.to) ? props.to : '',
+      date: (props.from != "")? {
+        from: addDays(new Date(props.from),1),
+        to: addDays(new Date(props.to),1),
+      }: {
+        from: new Date(),
+        to: addDays(new Date(), 20),
+      }
+    }
+  }
+
+  setDate = (date: DateRange|undefined) => {
+    if (date) {
+      this.setState({ date })
+    }
+  }
+
+  render() {
+    return (
+      <div className={cn("grid gap-2 z-50", this.props.className)}>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="date"
+              variant={"outline"}
+              className={cn(
+                "w-[300px] justify-start text-left font-normal",
+                !this.state.date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {this.state.date?.from ? (
+                this.state.date.to ? (
+                  <>
+                    {format(this.state.date.from, "LLL dd, y")} -{" "}
+                    {format(this.state.date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(this.state.date.from, "LLL dd, y")
+                )
+              ) : (
+                <span>Pick a date</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              initialFocus
+              mode="range"
+              defaultMonth={this.state.date.from}
+              selected={this.state.date}
+              onSelect={(date) => this.setDate(date)}
+              numberOfMonths={2}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+    )
+  }
+}
+
+export default DatePickerWithRange
